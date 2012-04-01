94d91a1e09eb223ed3d77171ba5f1b775817b343
julianshen <julianshen@gmail.com>
2012-04-02 01:40:20 +0800

    remove task temporarily

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,8 +1,5 @@
 package controllers;
 
-import java.util.List;
-
-import models.Task;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
@@ -10,7 +7,7 @@ import views.html.index;
 public class Application extends Controller {
   
   public static Result index() {
-	  List<Task> tasks = Task.find.all();
+	 // List<Task> tasks = Task.find.all();
     return ok(index.render("Your new application is ready. Test1!!"));
   }
   

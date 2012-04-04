d1d396096647decf776b1062ae0dc718a3cf0643
julianshen <julianshen@gmail.com>
2012-04-04 23:51:41 +0800

    add youtube test code

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,6 +9,7 @@ import play.mvc.Result;
 import views.html.index;
 
 import com.google.gdata.client.youtube.YouTubeService;
+import com.google.gdata.data.youtube.VideoEntry;
 import com.google.gdata.data.youtube.VideoFeed;
 import com.google.gdata.util.ServiceException;
 
@@ -22,10 +23,15 @@ public class Application extends Controller {
   public static Result youtubeTop() {
 	YouTubeService service = new YouTubeService("MyTest", "AI39si4w2Vv87Y-7KTFFZEVyL8_ylah-sxx2YHVEONVtMcW1et3JvHHXf1ew_RdUqihgvCv6bHATIfoPPaCCsuiaeD_hdCh69w");
 	VideoFeed videoFeed;
+	String testData="No Data";
 	try {
 		
 		videoFeed = service.getFeed(new URL("https://gdata.youtube.com/feeds/api/standardfeeds/top_rated"), VideoFeed.class);
 
+		testData = "";
+		for(VideoEntry videoEntry : videoFeed.getEntries() ) {
+		     testData += videoEntry.getHtmlLink().getHref()+",";
+		  }
 	} catch (MalformedURLException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
@@ -37,7 +43,7 @@ public class Application extends Controller {
 		e.printStackTrace();
 	}
 	
-	return ok(""); 
+	return ok(testData); 
   }
   
 }

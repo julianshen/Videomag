b874eb166ad2ae095c6627a9e1800718abc455df
julianshen <julianshen@gmail.com>
2012-04-04 23:44:45 +0800

    add media jar

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,43 @@
 package controllers;
 
-import java.util.List;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-import models.Task;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
+import com.google.gdata.client.youtube.YouTubeService;
+import com.google.gdata.data.youtube.VideoFeed;
+import com.google.gdata.util.ServiceException;
+
 public class Application extends Controller {
   
   public static Result index() {
-	List<Task> tasks = Task.find.all();
+	//List<Task> tasks = Task.find.all();
     return ok(index.render("Your new application is ready. Test1!!"));
+  }
+  
+  public static Result youtubeTop() {
+	YouTubeService service = new YouTubeService("MyTest", "AI39si4w2Vv87Y-7KTFFZEVyL8_ylah-sxx2YHVEONVtMcW1et3JvHHXf1ew_RdUqihgvCv6bHATIfoPPaCCsuiaeD_hdCh69w");
+	VideoFeed videoFeed;
+	try {
+		
+		videoFeed = service.getFeed(new URL("https://gdata.youtube.com/feeds/api/standardfeeds/top_rated"), VideoFeed.class);
+
+	} catch (MalformedURLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (ServiceException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	return ok(""); 
   }
   
 }
